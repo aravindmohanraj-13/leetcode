f8f069648401afc02e0bb46561922928c43262a8
Aravind M <aravindmohanraj2006@gmail.com>
2026-07-24 05:43:10 +0000

    Time: 2 ms (99.3%), Space: 44.5 MB (56.01%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,16 +1,29 @@
 class Solution {
-    public boolean isPalindrome(String s) {
-        int left = 0, right = s.length() - 1;
-        while (left < right) {
-            while (left < right && !Character.isLetterOrDigit(s.charAt(left))) left++;
-            while (left < right && !Character.isLetterOrDigit(s.charAt(right))) right--;
-
-            if (Character.toLowerCase(s.charAt(left)) != 
-                Character.toLowerCase(s.charAt(right))) {
-                return false;
+    boolean isAlphaNum(char s){
+        if((s >= 'a' && s <= 'z') || (s >= 'A' && s <= 'Z') ||
+            (s >= '0' && s <= '9') ){
+                return true;
             }
-            left++;
-            right--;
+        return false;
+    }
+
+    public boolean isPalindrome(String s) {
+        int left = 0;
+        int right = s.length() - 1;
+        while(left < right){
+            char l = s.charAt(left);
+            char r = s.charAt(right);
+            if(isAlphaNum(l) && isAlphaNum(r)){
+                if(Character.toLowerCase(l) != Character.toLowerCase(r)){
+                    return false;
+                }else{
+                    left++;
+                    right--;
+                }
+            }else{
+                if(!isAlphaNum(l)) left++;
+                if(!isAlphaNum(r)) right--;
+            }
         }
         return true;
     }
